13b33642516116b4a70c52b14997361ce97433f1
GaleevArslanDev <115714847+GaleevArslanDev@users.noreply.github.com>
2025-07-22 14:27:05 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import EditGamePage from '@/components/EditGamePage.vue'
 import GamesPage from '@/components/Games/GamesPage.vue'
 import LoginPage from '@/components/LoginPage.vue'

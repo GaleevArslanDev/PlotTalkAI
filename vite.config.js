b31d21e4effe9f0cedb3bd182fde2edf6ad801b4
GaleevArslanDev <115714847+GaleevArslanDev@users.noreply.github.com>
2025-07-22 12:16:07 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 import fs from 'fs'
 import path from 'path'
-
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(), 
+    vueDevTools(),
+    nodePolyfills({
+      include: ['crypto'],  // Явно включаем полифил для crypto
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
